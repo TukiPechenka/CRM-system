e4b7f43b4c2dfa3edac95622672a4b65919aee0f
Александр Грищнеко <tukipechenka@gmail.com>
2024-08-04 12:26:08 +1000

    Migrations + product views

--- crm/crmsystem/urls.py
+++ crm/crmsystem/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
 
+from .views import ProductListView, ProductCreateView, ProductDetailView, ProductDeleteView, ProductUpdateView
+
 
 app_name = 'crmsystem'
 
 urlpatterns = [
     path('login/', LoginView.as_view(
         template_name='registration/login.html', redirect_authenticated_user=True,
-    ))
+    ), name='login'),
+    path('products/', ProductListView.as_view(), name='products-list'),
+    path('products/new/', ProductCreateView.as_view(), name='products-create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='products-detail'),
+    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='products-update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='products-delete'),
 ]
--- crm/crmsystem/views.py
+++ crm/crmsystem/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Product, Ad, Lead, Contract, Customer
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products-list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/products-detail.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = 'name', 'description', 'cost'
+    template_name = 'products/products-create.html'
+
+    def get_success_url(self):
+        return reverse('crm:products-detail', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = 'name', 'description', 'cost'
+    template_name = 'products/products-edit.html'
+
+    def get_success_url(self):
+        return reverse('crm:products-detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'products/products-delete.html'
+    success_url = reverse_lazy('crm:products-list')
+
+
